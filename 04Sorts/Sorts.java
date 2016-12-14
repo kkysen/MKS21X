27af2edd2dd428eff8db67842f68d68767a6e0c1
Khyber Sen <kkysen@gmail.com>
2016-12-13 21:36:34 -0500

    fixed mistakes; selection, insertion, quick now done; merge not finished

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,16 +1,31 @@
+//package sen.khyber.apcs.sorts;
+
+//import sen.khyber.util.Timer;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.function.Consumer;
+
+/**
+ * 
+ * 
+ * @author Khyber Sen
+ */
 public class Sorts {
+    
+    private static Random random = new Random();
     
     public static String name() {
         return "06.Last.First";
     }
     
-    private static void swap(int[] a, int i, int j) {
-        int temp = i;
+    private static void swap(final int[] a, final int i, final int j) {
+        final int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
     
-    private static void minIndex(int[] a, int startIndex, int endIndex) {
+    private static int minIndex(final int[] a, final int startIndex, final int endIndex) {
         int min = a[startIndex];
         int val;
         int index = startIndex;
@@ -23,40 +38,44 @@ public class Sorts {
         return index;
     }
     
-    public static void selectionSort(int[] a) {
-        int len = a.length;
+    public static void selectionSort(final int[] a) {
+        final int len = a.length;
         for (int i = 0; i < len; i++) {
             swap(a, i, minIndex(a, i, len));
         }
     }
     
-    private static void insertIndex(int[] a, int index) {
-        int val = a[index];
+    private static int insertionIndex(final int[] a, final int index) {
+        final int val = a[index];
+        //        System.out.println("val: " + val);
         for (int i = index - 1; i >= 0; i--) {
-            if (val < a[i]) {
-                return i;
-            }    
+            //            System.out.println("a[i]: " + a[i]);
+            if (val > a[i]) {
+                return i + 1;
+            }
         }
-        return index;
+        return 0;
     }
     
-    private static void insert(int[] a, int index) {
-        int insertIndex = insertIndex(index);
-        int val = a[index];
-        System.arraycopy(a, insertIndex, a, insertIndex + 1, index - insertIndex);
-        a[insertIndex] = val;
+    private static void insert(final int[] a, final int index) {
+        final int insertionIndex = insertionIndex(a, index);
+        //        System.out.println("index: " + index);
+        //        System.out.println("insertionIndex: " + insertionIndex);
+        final int val = a[index];
+        System.arraycopy(a, insertionIndex, a, insertionIndex + 1, index - insertionIndex);
+        a[insertionIndex] = val;
     }
     
-    public static void insertionSort(int[] a) {
-        int len = a.length;
+    public static void insertionSort(final int[] a) {
+        final int len = a.length;
         for (int i = 1; i < len; i++) {
             insert(a, i);
         }
     }
     
-    private static void quickSort(int[] a, int left, int right) {
+    private static void quickSort(final int[] a, final int left, final int right) {
         int i = left, j = right;
-        int pivot = a[(i + j) / 2];
+        final int pivot = a[(i + j) / 2];
         int temp;
         while (i <= j) {
             while (a[i] < pivot) {
@@ -66,25 +85,25 @@ public class Sorts {
                 j--;
             }
             if (i <= j) {
-                temp = arr[i];
-                a[i++] = arr[j];
+                temp = a[i];
+                a[i++] = a[j];
                 a[j--] = temp;
             }
         }
         if (left < j) {
-            sort(a, left, j);
+            quickSort(a, left, j);
         }
         if (i < right) {
-            sort(a, i, right);
+            quickSort(a, i, right);
         }
     }
     
-    public static void quickSort(int[] a) {
-        quickSort(a, 0, arr.length - 1);
+    public static void quickSort(final int[] a) {
+        quickSort(a, 0, a.length - 1);
     }
     
-    private static int[] mergeSort(int[] a, int[] b) {
-        int[] merged = new int[a.length + b.length];
+    private static int[] mergeSort(final int[] a, final int[] b) {
+        final int[] merged = new int[a.length + b.length];
         int i = 0;
         int j = 0;
         int aVal;
@@ -92,7 +111,7 @@ public class Sorts {
         for (int k = 0; k < merged.length; k++) {
             if ((aVal = a[i]) < (bVal = b[j])) {
                 merged[k] = aVal;
-                i++
+                i++;
             } else {
                 merged[k] = bVal;
                 j++;
@@ -102,10 +121,59 @@ public class Sorts {
     }
     
     public static void mergeSort(int[] a) {
-        int splitIndex = a.length / 2;
-        int[] b = Arrays.copyOfRange(a, splitIndex, a.length);
+        final int splitIndex = a.length / 2;
+        final int[] b = Arrays.copyOfRange(a, splitIndex, a.length);
         a = Arrays.copyOfRange(a, 0, splitIndex);
         
+    }
+    
+    private static int[] warmUp() {
+        int[] a = random.ints(1_000_000).toArray();
+        for (int i = 0; i < 10; i++) {
+            Arrays.sort(a);
+            a = random.ints(1_000_000).toArray();
+        }
+        Arrays.sort(a);
+        return a;
+    }
+    
+    private static void testNoWarmUp(final Consumer<int[]> testSorter,
+            final Consumer<int[]> baselineSorter, final int size) {
+        final int[] a1 = random.ints(size).toArray();
+        final int[] a2 = Arrays.copyOf(a1, a1.length);
+//        Timer.start();
+        testSorter.accept(a1);
+//        final long testTime = Timer.time();
+//        Timer.printlnTime("testTime");
+//        Timer.start();
+        testSorter.accept(a2);
+//        final long baselineTime = Timer.time();
+//        Timer.printlnTime("baselineTime");
+//        System.out.println((double) testTime / baselineTime + " slower");
+        if (!Arrays.equals(a1, a2)) {
+            throw new AssertionError("sorting algorithm failed");
+        }
+    }
+    
+    public static void test(final Consumer<int[]> testSorter, final Consumer<int[]> baselineSorter,
+            final int size) {
+        warmUp();
+        testNoWarmUp(testSorter, baselineSorter, size);
+    }
+    
+    public static void test(final Consumer<int[]> sorter, final int size) {
+        test(sorter, Arrays::sort, size);
+    }
+    
+    public static void print(final int[] a) {
+        System.out.println(Arrays.toString(a));
+    }
+    
+    public static void main(final String[] args) {
+        test(Sorts::insertionSort, Arrays::sort, 500000);
+        final int[] a = new int[] {3, 5, 1, 6, 8, 3, 9};
+//        insertionSort(a);
+//        print(a);
     }
     
 }
