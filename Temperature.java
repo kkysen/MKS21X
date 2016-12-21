63d14fbc2144976731e4b302f0e76c84f624aa26
Khyber Sen <kkysen@gmail.com>
2016-12-21 02:34:49 -0500

    formatted Temperature

--- Temperature.java
+++ Temperature.java
@@ -1,14 +1,21 @@
+//package sen.khyber.apcs;
+
+/**
+ * 
+ * 
+ * @author Khyber Sen
+ */
 public class Temperature {
     
-    public static double CtoF(double t) {
+    public static double CtoF(final double t) {
         return t * 9 / 5 + 32;
     }
     
-    public static double FtoC(double t) {
+    public static double FtoC(final double t) {
         return (t - 32) * 5 / 9;
     }
     
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println(CtoF(-40));
         System.out.println(CtoF(100));
         System.out.println(FtoC(212));
